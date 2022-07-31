f84ab40a966471753866b81afe7d850e6017c139
Gabriel <foioamaral16@gmail.com>
2022-07-31 14:12:13 -0300

    feat: finished basic header component

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import { Header } from "../../components/header";
+
+export const Home = () => {
+  return (
+    <Box top="10" minH="100vh" w="100%" backgroundColor="red">
+      <Header />
+    </Box>
+  );
+};
---
+++ src/routes/Route.tsx
@@ -0,0 +1,29 @@
+import { ComponentType } from "react";
+import { Redirect, Route as ReactRoute, RouteProps } from "react-router-dom";
+import { useUser } from "../providers/UserProvider";
+
+interface Props extends RouteProps {
+  isPrivate?: boolean;
+  component: ComponentType;
+}
+
+export const Route = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}: Props) => {
+  const { token } = useUser();
+
+  return (
+    <ReactRoute
+      {...rest}
+      render={() =>
+        isPrivate === !!token ? (
+          <Component />
+        ) : (
+          <Redirect to={isPrivate ? "/" : "/user"} />
+        )
+      }
+    />
+  );
+};
